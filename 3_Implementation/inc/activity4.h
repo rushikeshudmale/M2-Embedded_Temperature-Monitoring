193c24bc32d3c11454202ad4474e0075c3486d6e
rushikeshudmale <94127613+rushikeshudmale@users.noreply.github.com>
2021-12-01 16:02:32 +0530

    updated readme

---
+++ 3_Implementation/inc/activity4.h
@@ -0,0 +1,44 @@
+#ifndef __ACTIVITY_4__
+#define __ACTIVITY_4__
+
+/**
+ * Include files
+ */ 
+#include "project_config.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+/**
+ * Macro definitions
+ */
+#define BAUD 9600 /**< Baud rate to 9600 */
+#define BAUDRATE (F_CPU/(BAUD*16UL)-1) /**< Baud rate conversion factor */
+
+/**
+ * @brief Initialize all the Peripherals and pin configurations
+ * 
+ */
+void activity4_init();
+
+/**
+ * @brief Writes a character on tx pin
+ * 
+ * @param ch 
+ */
+void write_ch_tx(char ch);
+
+/**
+ * @brief Reads a character on rx pin
+ * 
+ * @return Returns the read character 
+ */
+char read_ch_rx();
+
+/**
+ * @brief Writes a line on tx pin
+ * 
+ * @param str 
+ */
+void write_tx(char *str);
+
+#endif /** __ACTIVITY4_H__ */
